5de0ef3a8d2c852ef524ede5d746b01af239c705
akh-ild <ildar.akhmadeyev@gmail.com>
2025-03-25 20:30:32 +0500

    init

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log('Robot Bobby was here');
+import * as THREE from "three";
+import { OrbitControls } from 'jsm/controls/OrbitControls.js';
+
+const w = window.innerWidth;
+const h = window.innerHeight;
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
+camera.position.z = 5;
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(w, h);
+document.body.appendChild(renderer.domElement);
+
+new OrbitControls(camera, renderer.domElement);
+
+
+const geometry = new THREE.IcosahedronGeometry(1, 12);
+const material = new THREE.MeshStandardMaterial({
+  color: 0xffff00,
+  // flatShading: true,
+ });
+ const earthMesh = new THREE.Mesh(geometry, material);
+ scene.add(earthMesh);
+
+ const hemiLight = new THREE.HemisphereLight(0x0099ff, 0xaa5500);
+ scene.add(hemiLight);
+
+ function animate(t = 0) {
+   requestAnimationFrame(animate);
+   earthMesh.rotation.x += 0.001;
+   earthMesh.rotation.y += 0.002;
+   renderer.render(scene, camera);
+ };
+
+ animate();
