c629a7f486ab7b4890efd906f883f51aa0be3e6e
akh-ild <ildar.akhmadeyev@gmail.com>
2025-03-26 21:17:19 +0500

    stars, earth texture

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from 'jsm/controls/OrbitControls.js';
+import getStarField from "./src/starField.js";
 
 const w = window.innerWidth;
 const h = window.innerHeight;
@@ -11,25 +12,30 @@ const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
 
+const earthGroup = new THREE.Group();
+earthGroup.rotation.z = -23.4 * Math.PI / 180;
+scene.add(earthGroup);
+
 new OrbitControls(camera, renderer.domElement);
 
-
+const loader = new THREE.TextureLoader();
 const geometry = new THREE.IcosahedronGeometry(1, 12);
 const material = new THREE.MeshStandardMaterial({
-  color: 0xffff00,
-  // flatShading: true,
- });
- const earthMesh = new THREE.Mesh(geometry, material);
- scene.add(earthMesh);
+  map: loader.load("./textures/00_earthmap1k.jpg"),
+});
+const earthMesh = new THREE.Mesh(geometry, material);
+earthGroup.add(earthMesh);
 
- const hemiLight = new THREE.HemisphereLight(0x0099ff, 0xaa5500);
- scene.add(hemiLight);
+const stars = getStarField({ numStars: 10000 });
+scene.add(stars);
 
- function animate(t = 0) {
-   requestAnimationFrame(animate);
-   earthMesh.rotation.x += 0.001;
-   earthMesh.rotation.y += 0.002;
-   renderer.render(scene, camera);
- };
+const hemiLight = new THREE.HemisphereLight(0x0099ff, 0xaa5500);
+scene.add(hemiLight);
 
- animate();
+function animate(t = 0) {
+  requestAnimationFrame(animate);
+//  earthMesh.rotation.y += 0.002;
+  renderer.render(scene, camera);
+};
+
+animate();
