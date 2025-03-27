2f32f8044fa1506529077e2fb3250eac339c4f9a
akh-ild <ildar.akhmadeyev@gmail.com>
2025-03-27 20:02:54 +0500

    lightsMesh, cloudsMesh

--- index.js
+++ index.js
@@ -18,23 +18,44 @@ scene.add(earthGroup);
 
 new OrbitControls(camera, renderer.domElement);
 
+const detail = 12;
 const loader = new THREE.TextureLoader();
-const geometry = new THREE.IcosahedronGeometry(1, 12);
+const geometry = new THREE.IcosahedronGeometry(1, detail);
 const material = new THREE.MeshStandardMaterial({
   map: loader.load("./textures/00_earthmap1k.jpg"),
 });
+
 const earthMesh = new THREE.Mesh(geometry, material);
 earthGroup.add(earthMesh);
+
+const lightsMat = new THREE.MeshBasicMaterial({
+  map: loader.load("./textures/03_earthlights1k.jpg"),
+  blending: THREE.AdditiveBlending,
+ });
+const lightsMesh = new THREE.Mesh(geometry, lightsMat);
+earthGroup.add(lightsMesh);
+
+const cloudMat = new THREE.MeshStandardMaterial({
+  map: loader.load("./textures/05_earthcloudmaptrans.jpg"),
+  transparent: true,
+  opacity: 0.8,
+  blending: THREE.AdditiveBlending,
+});
+const cloudsMesh = new THREE.Mesh(geometry, cloudMat);
+earthGroup.add(cloudsMesh);
 
 const stars = getStarField({ numStars: 10000 });
 scene.add(stars);
 
-const hemiLight = new THREE.HemisphereLight(0x0099ff, 0xaa5500);
-scene.add(hemiLight);
+const sunLight = new THREE.DirectionalLight(0xffffff, 0.5);
+sunLight.position.set(-2, 0.5, 1.5);
+scene.add(sunLight);
 
 function animate(t = 0) {
   requestAnimationFrame(animate);
-//  earthMesh.rotation.y += 0.002;
+  earthMesh.rotation.y += 0.002;
+  lightsMesh.rotation.y += 0.002;
+  cloudsMesh.rotation.y += 0.002;
   renderer.render(scene, camera);
 };
 
